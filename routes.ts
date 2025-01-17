efadbe61fa57dbe50528be420aff42aaf3ca0774
CoreyRobinsonDev <coreyrob52@gmail.com>
2025-01-16 23:36:39 -0500

    feat: grab message content from chat

--- routes.ts
+++ routes.ts
@@ -30,37 +30,73 @@ export async function getKickChat(
 						message: tab.page.unwrapErr().message
 					}))
 					controller.close()
+					TC.leaveTab(tab)
 				}
 				controller.enqueue(JSON.stringify({
 					status: 200,
 					code: Code.CON,
 					message: `Connected to ${streamer} chatroom...`
 				}))
+				let chat: any
 				timer = setInterval(async () => {
-					let chat = await tab.page?.unwrap().$$eval("div.chat-entry > div", chats => {
-						return chats.map(el => {
-							const badgeImg = el.querySelector("img.icon")?.getAttribute("src")
-							const badgeName = el.querySelector("img.icon")?.getAttribute("alt")
-							const userName = el.querySelector(".chat-entry-username")?.textContent
-							const userColor = el.querySelector(".chat-entry-username")?.getAttribute("style")
-								?.split("(").at(-1)
-								?.split(",")
-								.slice(0, 3)
-								.map((el, idx) => {
-									if (idx === 2) {
-										return Number(el.trim().split(")")[0])
+					try {
+						chat = await tab.page?.unwrap().$$eval("div.chat-entry > div", chats => {
+							return chats.map(el => {
+								const badgeImg = el.querySelector("img.icon")?.getAttribute("src")
+								const badgeName = el.querySelector("img.icon")?.getAttribute("alt")
+								const userName = el.querySelector(".chat-entry-username")?.textContent
+								const userColor = el.querySelector(".chat-entry-username")?.getAttribute("style")
+									?.split("(").at(-1)
+									?.split(",")
+									.slice(0, 3)
+									.map((el, idx) => {
+										if (idx === 2) {
+											return Number(el.trim().split(")")[0])
+										}
+										return Number(el.trim())
+									})
+								const contentHTML = el.querySelector(".font-bold.text-white + span")?.children
+								let content = ""
+								let emoteContainer: {
+									[U: string]: string
+								} = {
+
+								}
+								for (let i = 0; i < (contentHTML?.length ?? 0); i++) {
+									const className = contentHTML?.item(i)?.querySelector(".chat-emote-container, .chat-entry-content")?.className
+									if (className === "chat-emote-container") {
+										const emoteName = contentHTML?.item(i)?.querySelector(".chat-emote-container > div > img")?.getAttribute("alt")
+										const emoteSrc = contentHTML?.item(i)?.querySelector(".chat-emote-container > div > img")?.getAttribute("src")
+										if (content.length > 0) {
+											content += " " + emoteName
+										} else { content += emoteName }
+										if (typeof emoteName === "string") {
+											//@ts-ignore: ts doesn't know what ^this^ means
+											emoteContainer[emoteName] = emoteSrc
+										}
+									} else if (className === "chat-entry-content") {
+										if (content.length > 0) {
+											content += " " + contentHTML?.item(i)?.querySelector(".chat-entry-content")?.textContent
+										} else {
+											content += contentHTML?.item(i)?.querySelector(".chat-entry-content")?.textContent
+										}
 									}
-									return Number(el.trim())
-								})
-							return {
-								badgeName,
-								badgeImg,
-								userName,
-								userColor
-							}
+								}
+
+								return {
+									badgeName,
+									badgeImg,
+									userName,
+									userColor,
+									content,
+									emoteContainer
+								}
+							})
 						})
-					})
-					console.log(chat)
+						console.log(chat)
+					} catch(_) {
+						console.error("Puppeteer Error Occurred")
+					}
 					// const diffChats = diff(prevChat, chat ?? [])
 					// prevChat = chat ?? []
 					// chat = diffChats
@@ -72,7 +108,9 @@ export async function getKickChat(
 							code: Code.MSG,
 							message: chat
 						}))
-					} catch(_) { }
+					} catch(_) { 
+						console.error("Enqueue Error Occurred")
+					}
 				}, 1000)
 			},
 			cancel() {
