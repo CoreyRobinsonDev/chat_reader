135aae174428e6849e61d58f03f1450eebe902f3
CoreyRobinsonDev <coreyrob52@gmail.com>
2025-01-26 15:47:04 -0500

    feat: add Test component

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client"
 import App from "./app"
+import Test from "./Test"
 
 document.addEventListener("DOMContentLoaded", () => {
 	const root = createRoot(document.getElementById("root")!)
-	root.render(<App/>)
+	root.render(<Test/>)
 })
