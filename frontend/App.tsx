a8d3f9c06089a6b53f26f1a2e68a64a7d150a092
CoreyRobinsonDev <coreyrob52@gmail.com>
2025-03-08 12:25:46 -0500

    feat: update streamer search bar

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client"
 import KickChat from "./components/KickChat"
 import { useState, type JSX } from "react"
-import { FaTwitch, FaTwitter, FaYoutube } from "react-icons/fa"
+import { FaTwitch, FaTwitter, FaYoutube, FaSearch } from "react-icons/fa"
 import { SiKick } from "react-icons/si"
 
-type Icon = {
-    [U: string]: JSX.Element
-}
 
 function App() {
     const [selectedPlatform, setSelectedPlatform] = useState(localStorage.getItem("platform") ?? "TWITCH")
-    const icon: Icon = {
+    const [streamer, setStreamer] = useState("")
+    const [streamerList, setStreamerList] = useState<{streamer: string, platform: string}[]>([])
+
+    const icon: {[U: string]: JSX.Element} = {
         TWITCH: <FaTwitch />,
         TWITTER: <FaTwitter />,
         KICK: <SiKick />,
@@ -31,11 +31,16 @@ function App() {
                     <span className="icon">
                         {icon[selectedPlatform]}
                     </span>
-                    <input 
-                        className="streamer-input"
-                        type="text" 
-                        placeholder="Find streamer..." 
-                    />
+                    <form className="streamer-form">
+                        <input 
+                            className="streamer-input"
+                            type="text" 
+                            placeholder="Find streamer..." 
+                            value={streamer}
+                            onChange={(e) => setStreamer(e.target.value)}
+                        />
+                        <button type="submit"><FaSearch /></button>
+                    </form>
                 </div>
                 <ul className="icon_menu">
                     <li data-platform="TWITCH" className="icon_menu_item" onClick={() => setPlatform("TWITCH")}><span>{icon.TWITCH}</span> </li>
@@ -44,8 +49,11 @@ function App() {
                     <li data-platform="YOUTUBE" className="icon_menu_item" onClick={() => setPlatform("YOUTUBE")}><span>{icon.YOUTUBE}</span> </li>
                 </ul>
             </div>
-       </header>
+        </header>
         <section className="chat-container">
+            <ul>
+
+            </ul>
         </section>
     </div>
 }
