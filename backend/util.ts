729ced7a8d8bdd436e963522a012960901330b2d
CoreyRobinsonDev <coreyrob52@gmail.com>
2025-03-09 14:19:37 -0400

    fix: correct day and zero pad logs

--- backend/util.ts
+++ backend/util.ts
@@ -56,14 +56,32 @@ export const Resp = {
 export const log = {
     info: (msg: string) => {
         const date = new Date()
-        console.log(`\x1b[90m[${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]\x1b[0m \x1b[34mINF\x1b[0m : ${msg}`)
+        const hour = ('0'+date.getHours()).slice(-2)
+        const min = ('0'+date.getMinutes()).slice(-2)
+        const sec = ('0'+date.getSeconds()).slice(-2)
+        const mon = ('0'+date.getMonth()).slice(-2)
+        const day = ('0'+date.getDate()).slice(-2)
+
+        console.log(`\x1b[90m[${date.getFullYear()}/${mon}/${day} ${hour}:${min}:${sec}]\x1b[0m \x1b[34mINF\x1b[0m : ${msg}`)
     },
     debug: (msg: string) => {
         const date = new Date()
-        console.log(`\x1b[90m[${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]\x1b[0m \x1b[33mDBG\x1b[0m : ${msg}`)
+        const hour = ('0'+date.getHours()).slice(-2)
+        const min = ('0'+date.getMinutes()).slice(-2)
+        const sec = ('0'+date.getSeconds()).slice(-2)
+        const mon = ('0'+date.getMonth()).slice(-2)
+        const day = ('0'+date.getDate()).slice(-2)
+
+        console.log(`\x1b[90m[${date.getFullYear()}/${mon}/${day} ${hour}:${min}:${sec}]\x1b[0m \x1b[33mDBG\x1b[0m : ${msg}`)
     },
     error: (msg: string) => {
         const date = new Date()
-        console.error(`\x1b[90m[${date.getFullYear()}/${date.getMonth()}/${date.getDay()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}]\x1b[0m \x1b[31mERR\x1b[0m : ${msg}`)
+        const hour = ('0'+date.getHours()).slice(-2)
+        const min = ('0'+date.getMinutes()).slice(-2)
+        const sec = ('0'+date.getSeconds()).slice(-2)
+        const mon = ('0'+date.getMonth()).slice(-2)
+        const day = ('0'+date.getDate()).slice(-2)
+
+        console.error(`\x1b[90m[${date.getFullYear()}/${mon}/${day} ${hour}:${min}:${sec}]\x1b[0m \x1b[31mERR\x1b[0m : ${msg}`)
     },
 }
