242cc26117709348d7c2a2fb18d137a7d4a36ef7
CoreyRobinsonDev <coreyrob52@gmail.com>
2025-04-13 18:33:46 -0400

    feat: add scraping of twitch chats

--- backend/scrape.ts
+++ backend/scrape.ts
@@ -1,12 +1,19 @@
 import { Browser, executablePath, Page, type LaunchOptions } from "puppeteer";
 import stealthPlugin from "puppeteer-extra-plugin-stealth"
 import puppeteer from "puppeteer-extra"
-import { type Chat } from "./types";
+import { Platform, type Chat } from "./types";
+import path from "path";
 
 
-const MAX_TIMEOUT: number = 10_000
+const MAX_TIMEOUT: number = 30_000
+const extension = `${path.join(process.cwd(), "assets/extensions/7tv")}`
 const CONFIG: LaunchOptions = {
-	args: ["--no-sandbox", "--disable-setuid-sandbox"],
+	args: [
+        "--no-sandbox", 
+        "--disable-setuid-sandbox",
+        `--disable-extensions-except=${extension}`,
+        `--load-extension=${extension}`
+    ],
 	defaultViewport: { width: 1980, height: 1024 },
 	slowMo: 50, 
 	executablePath: executablePath(),
@@ -14,8 +21,7 @@ const CONFIG: LaunchOptions = {
 }
 
 export async function kick(page: Page): Promise<Chat[]> {
-	let chat: Chat[]
-	chat = await page.$$eval("div.chat-entry > div", chats => {
+	const chat  = await page.$$eval("div.chat-entry > div", chats => {
 		return chats.map(el => {
 			const badgeImg = el.querySelector("img.icon")?.getAttribute("src")
 			const badgeName = el.querySelector("img.icon")?.getAttribute("alt")
@@ -66,6 +72,79 @@ export async function kick(page: Page): Promise<Chat[]> {
 	}) ?? []
 
 	return chat.reverse().filter(el => el.userName !== "ERR" || el.content.length !== 0)
+}
+
+export async function twitch(page: Page): Promise<Chat[]> {
+    const chat = await page.$$eval("main.seventv-chat-list > div", chats => {
+        return chats.map(el => {
+			const badgeImg = el.querySelector(".seventv-chat-badge > img")?.getAttribute("srcset")?.split(",").at(-1)?.split(" ")[1]
+			const badgeName = el.querySelector(".seventv-chat-badge > img")?.getAttribute("alt")
+			const userName = el.querySelector(".seventv-chat-user-username")?.textContent
+			const userColor = el.querySelector(".seventv-chat-user")?.getAttribute("style")
+				?.split("(").at(-1)
+				?.split(",")
+				.slice(0, 3)
+				.map((el, idx) => {
+					if (idx === 2) {
+						return Number(el.trim().split(")")[0])
+					}
+					return Number(el.trim())
+				})
+			const contentHTML = el.querySelectorAll(".text-token, .emote-token, .mention-token")
+            let content = ""
+			let emoteContainer: Chat["emoteContainer"] = {}
+			for (let i = 0; i < (contentHTML?.length ?? 0); i++) {
+				if (contentHTML.item(i).children.length !== 0) {
+					const emoteName = contentHTML.item(i).firstElementChild?.getAttribute("alt")
+					const emoteSrc = "https:" + contentHTML.item(i).firstElementChild?.getAttribute("srcset")
+                        ?.split(",").at(-1)?.split(" ")[1]
+					if (content.length > 0) {
+						content += " " + emoteName
+					} else { content += emoteName }
+					if (typeof emoteName === "string") {
+						emoteContainer[emoteName] = emoteSrc ?? "ERR"
+					}
+				} else if (contentHTML.item(i).className.includes("text-token") 
+                    || contentHTML.item(i).className.includes("mention-token") 
+                ) {
+					if (content.length > 0) {
+						content += " " + contentHTML.item(i).textContent
+					} else {
+						content += contentHTML.item(i).textContent
+					}
+				}
+			}
+
+			return {
+				badgeName: badgeName ? badgeName : undefined,
+				badgeImg: badgeImg ? badgeImg : undefined,
+				userName: userName ? userName : "ERR",
+				userColor: userColor ? userColor : [0,0,0],
+                // removing double spacing around emotes
+				content: content.split("  ").join(" ").trim(),
+				emoteContainer: Object.keys(emoteContainer).length > 0 ? emoteContainer : undefined
+			}
+        })
+    }) ?? []
+	return chat.reverse().filter(el => el.userName !== "ERR" || el.content.length !== 0)
+}
+
+export async function twitter(page: Page): Promise<Chat[]> {
+    return []
+}
+
+export async function youtube(page: Page): Promise<Chat[]> {
+    return []
+}
+
+
+export async function getChat(platform: Platform, page: Page): Promise<Chat[]> {
+    switch(platform) {
+    case Platform.TWITCH: return twitch(page)
+    case Platform.KICK: return kick(page)
+    case Platform.TWITTER: return twitter(page)
+    case Platform.YOUTUBE: return youtube(page)
+    }
 }
 
 export async function initBrowser(): Promise<Browser> {
--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-import { goto, initBrowser, kick } from "./backend/scrape.ts";
+import { getChat, goto, initBrowser, kick } from "./backend/scrape.ts";
 import { log, Resp, tryCatch, unwrap } from "./backend/util.ts";
-import { SocketCode, type WebSocketData, Platform } from "./backend/types.ts";
+import { SocketCode, type WebSocketData, Platform, type Result } from "./backend/types.ts";
 import index from "./frontend/index.html" 
 import type { RouterTypes } from "bun";
+import type { Page } from "puppeteer";
 
 export const BROWSER = unwrap(await tryCatch(initBrowser()))
 
@@ -20,7 +21,7 @@ const s = Bun.serve<WebSocketData, Routes>({
 
             if (!s.upgrade(req, {
                 data: {
-                    streamer: streamer.toUpperCase(), 
+                    streamer: streamer.toLowerCase(), 
                     platform: platform.toUpperCase(),
                     clientId: Bun.randomUUIDv7()
                 },
@@ -58,86 +59,13 @@ const s = Bun.serve<WebSocketData, Routes>({
 			ws.subscribe(platform+streamer)
 			if (s.subscriberCount(platform+streamer) > 1) return
 
-
+            let site = ""
 			switch (platform) {
             case Platform.TWITCH:
-                log.debug("twitch")
+                site = `https://twitch.tv/popout/${streamer}/chat`
                 break
 			case Platform.KICK:
-				const site = `https://kick.com/${streamer}/chatroom`
-				const [page, pageErr] = await tryCatch(goto(BROWSER, site))
-				let lastUsername = "" 
-                let lastContent = ""
-                let emptyResponses = 0
-                const emptyRepsonseLimit = 500
-
-				if (!page) {
-					ws.unsubscribe(platform+streamer)
-					ws.close(SocketCode.InternalServerError, `Error on visiting ${site}`)
-                    log.debug(`[${clientId}] has disconnected`)
-                    log.error(`\tError on visiting ${site}`)
-                    log.error(`\t${pageErr}`)
-					return
-				}
-
-				while (s.subscriberCount(platform+streamer) > 0) {
-					const [chat, chatErr] = await tryCatch(kick(page))
-
-					if (!chat) {
-						ws.unsubscribe(platform+streamer)
-						ws.close(SocketCode.InternalServerError, `Error on scraping ${site}`)
-						await page.close()
-                        log.debug(`[${clientId}] has disconnected`)
-                        log.error(`\t[${clientId}] Error on scraping ${site}`)
-                        log.error(`\t${chatErr}`)
-						return
-					}
-
-					if (chat.length === 0) {
-                        log.debug(`[${clientId}] has disconnected`)
-                        log.debug(`\t[${clientId}] ${platform} streamer ${streamer} is offline`)
-						ws.unsubscribe(platform+streamer)
-						ws.close(SocketCode.BadRequest, `${platform.toLowerCase()} streamer ${streamer.toLowerCase()} is offline`)
-						await page.close()
-						return
-					}
-
-					const idx = chat.findIndex(el => el.userName === lastUsername && el.content === lastContent)
-					if (idx === -1) {
-						if (chat.length === 0) {
-                            emptyResponses++
-                            if (emptyResponses >= emptyRepsonseLimit) {
-                                log.debug(`[${clientId}] has disconnected`)
-                                log.debug(`\t${platform} streamer ${streamer} is offline`)
-						        ws.unsubscribe(platform+streamer)
-                                ws.close(SocketCode.BadRequest, `${platform} streamer ${streamer} is offline`)
-                                await page.close()
-                                return
-                            }
-                            continue 
-                        }
-                        emptyResponses = 0
-						s.publish(platform+streamer, JSON.stringify(chat), true)
-					} else {
-						if (chat.slice(0, idx).length === 0) {
-                            emptyResponses++
-                            if (emptyResponses >= emptyRepsonseLimit) {
-                                log.debug(`[${clientId}] has disconnected`)
-                                log.debug(`\t${platform} streamer ${streamer} is offline`)
-                                ws.close(SocketCode.BadRequest, `${platform} streamer ${streamer} is offline`)
-                                await page.close()
-                                return
-                            }
-                            continue 
-                        }
-                        emptyResponses = 0
-						s.publish(platform+streamer, JSON.stringify(chat.slice(0, idx)), true)
-					}
-					lastUsername = chat[0].userName
-					lastContent = chat[0].content
-					await Bun.sleep(100)
-				}
-				await page.close()
+                site = `https://kick.com/${streamer}/chatroom`
                 break
             default:
                 ws.unsubscribe(platform+streamer)
@@ -145,6 +73,81 @@ const s = Bun.serve<WebSocketData, Routes>({
                 log.debug(`[${clientId}] has disconnected`)
                 log.debug(`\t[${clientId}] Call to ${platform} is unimplemented`)
 			}
+
+            const [page, pageErr] = await tryCatch(goto(BROWSER, site))
+            let lastUsername = "" 
+            let lastContent = ""
+            let emptyResponses = 0
+            const emptyRepsonseLimit = 500
+
+            if (!page) {
+                ws.unsubscribe(platform+streamer)
+                ws.close(SocketCode.InternalServerError, `Error on visiting ${site}`)
+                log.debug(`[${clientId}] has disconnected`)
+                log.error(`\tError on visiting ${site}`)
+                log.error(`\t${pageErr}`)
+                return
+            }
+
+            while (s.subscriberCount(platform+streamer) > 0) {
+                const [chat, chatErr] = await tryCatch(getChat(platform, page))
+
+                if (!chat) {
+                    ws.unsubscribe(platform+streamer)
+                    ws.close(SocketCode.InternalServerError, `Error on scraping ${site}`)
+                    await page.close()
+                    log.debug(`[${clientId}] has disconnected`)
+                    log.error(`\t[${clientId}] Error on scraping ${site}`)
+                    log.error(`\t${chatErr}`)
+                    return
+                }
+
+                if (chat.length === 0) {
+                    log.debug(`[${clientId}] has disconnected`)
+                    log.debug(`\t[${clientId}] ${platform} streamer ${streamer} is offline`)
+                    ws.unsubscribe(platform+streamer)
+                    ws.close(SocketCode.BadRequest, `${platform.toLowerCase()} streamer ${streamer.toLowerCase()} is offline`)
+                    await page.close()
+                    return
+                }
+
+                const idx = chat.findIndex(el => el.userName === lastUsername && el.content === lastContent)
+                if (idx === -1) {
+                    if (chat.length === 0) {
+                        emptyResponses++
+                        if (emptyResponses >= emptyRepsonseLimit) {
+                            log.debug(`[${clientId}] has disconnected`)
+                            log.debug(`\t${platform} streamer ${streamer} is offline`)
+                            ws.unsubscribe(platform+streamer)
+                            ws.close(SocketCode.BadRequest, `${platform} streamer ${streamer} is offline`)
+                            await page.close()
+                            return
+                        }
+                        continue 
+                    }
+                    emptyResponses = 0
+                    s.publish(platform+streamer, JSON.stringify(chat), true)
+                } else {
+                    if (chat.slice(0, idx).length === 0) {
+                        emptyResponses++
+                        if (emptyResponses >= emptyRepsonseLimit) {
+                            log.debug(`[${clientId}] has disconnected`)
+                            log.debug(`\t${platform} streamer ${streamer} is offline`)
+                            ws.close(SocketCode.BadRequest, `${platform} streamer ${streamer} is offline`)
+                            await page.close()
+                            return
+                        }
+                        continue 
+                    }
+                    emptyResponses = 0
+                    s.publish(platform+streamer, JSON.stringify(chat.slice(0, idx)), true)
+                }
+                lastUsername = chat[0].userName
+                lastContent = chat[0].content
+                await Bun.sleep(100)
+            }
+            await page.close()
+            return
 		},
 		async close(ws) {
 			ws.unsubscribe(ws.data.platform+ws.data.streamer)
